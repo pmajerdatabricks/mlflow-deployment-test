b5350e234e21769c3b0c70376147147709d4ef90
pmajerdatabricks <piotr.majer@databricks.com>
2020-04-16 16:34:02 +0200

    somehow the repo was empty

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='demoaws',
+    packages=find_packages(),
+    version='0.1.0',
+    description='MLflow Deployments Sample Project',
+    author='piotr majer',
+    license='',
+)
